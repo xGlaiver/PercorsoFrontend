ccbb83942acf5b84e88c4776748943345b67da40
xGlaiver <marcomantogame1998@hotmail.com>
2021-03-01 15:55:55 +0100

    Tutto sistemato

--- JS/script.js
+++ JS/script.js
@@ -13,14 +13,17 @@ const activePrice = (e) => {
     if(selected.classList.contains("left")) {
         document.querySelector("#moneyPrice").innerHTML = "50€";
         document.querySelector("#time").innerHTML = "1 mese";
+        document.querySelector("#linkProPrice").setAttribute("href", "https://sso.teachable.com/secure/418132/checkout/2727323/percorso-sviluppo-web-app");
     }
     else if(selected.classList.contains("center")) {
         document.querySelector("#moneyPrice").innerHTML = "120€";
         document.querySelector("#time").innerHTML = "3 mesi";
+        document.querySelector("#linkProPrice").setAttribute("href", "https://sso.teachable.com/secure/418132/checkout/2929386/percorso-sviluppo-web-app");
     }
     else if(selected.classList.contains("right")) {
         document.querySelector("#moneyPrice").innerHTML = "200€";
         document.querySelector("#time").innerHTML = "6 mesi";
+        document.querySelector("#linkProPrice").setAttribute("href", "https://sso.teachable.com/secure/418132/checkout/2929402/percorso-sviluppo-web-app");
     }
 }
 
@@ -39,13 +42,16 @@ const activePriceMobile = (e) => {
      if(selected.classList.contains("left")) {
          document.querySelector("#moneyPriceMobile").innerHTML = "50€";
          document.querySelector("#timeMobile").innerHTML = "1 mese";
+         document.querySelector("#linkProPrice").setAttribute("href", "https://sso.teachable.com/secure/418132/checkout/2727323/percorso-sviluppo-web-app");
      }
      else if(selected.classList.contains("center")) {
          document.querySelector("#moneyPriceMobile").innerHTML = "120€";
          document.querySelector("#timeMobile").innerHTML = "3 mesi";
+         document.querySelector("#linkProPrice").setAttribute("href", "https://sso.teachable.com/secure/418132/checkout/2929386/percorso-sviluppo-web-app");
      }
      else if(selected.classList.contains("right")) {
          document.querySelector("#moneyPriceMobile").innerHTML = "200€";
          document.querySelector("#timeMobile").innerHTML = "6 mesi";
+         document.querySelector("#linkProPrice").setAttribute("href", "https://sso.teachable.com/secure/418132/checkout/2929402/percorso-sviluppo-web-app");
      }
  }
